3bf016e5468542edf7b3b28ceacab8c1b2ded0b1
huangyuanzhi1997 <460013011@qq.com>
2020-09-01 22:11:40 +0800

    添加项目主程序启动类

---
+++ src/main/java/top/huangyuanzhi/StartApplication.java
@@ -0,0 +1,15 @@
+package top.huangyuanzhi;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("springboot.dao")
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class);
+
+    }
+}
